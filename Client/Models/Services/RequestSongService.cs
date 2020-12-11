ccab9ced71d18d4bf2308576c17bf93845581d13
SHINTA <shinta.0806@gmail.com>
2020-12-11 21:55:52 +0900

    リクエスト一覧の提供をサーバー側に移動（固定データ）

--- Client/Models/Services/RequestSongService.cs
+++ Client/Models/Services/RequestSongService.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using YukariBlazorDemo.Shared;
 
@@ -18,16 +20,27 @@ namespace YukariBlazorDemo.Client.Models.Services
 {
 	public class RequestSongService
 	{
+#if false
 		private List<RequestSong> RequestSongs { get; } = new()
 		{
 			new RequestSong { Id = 1, Sort = 1, Path = @"D:\Song\hoge.mp4", SongName = "Hoge", TieUpName = "HogeAnime", User = "Taro" },
 			new RequestSong { Id = 2, Sort = 2, Path = @"D:\Song\fuga.mp4", SongName = "Fuga", TieUpName = "FugaAnime", User = "Hanako" },
 			new RequestSong { Id = 3, Sort = 3, Path = @"D:\Song\foo.mp4", SongName = "Foo", TieUpName = "FooAnime", User = "Miyuki" },
 		};
+#endif
 
-		public IEnumerable<RequestSong> GetRequestSongs()
+		public HttpClient HttpClient { get; }
+
+		public RequestSongService(HttpClient httpClient)
 		{
-			return RequestSongs;
+			HttpClient = httpClient;
+		}
+
+		public async Task<IEnumerable<RequestSong>> GetRequestSongsAsync()
+		{
+			//return await Task.FromResult(RequestSongs);
+
+			return await HttpClient.GetFromJsonAsync<RequestSong[]>("api/requestsongs");
 		}
 	}
 }
---
+++ Server/Controllers/RequestSongController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YukariBlazorDemo.Shared;
+
+namespace YukariBlazorDemo.Server.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/requestsongs")]
+	public class RequestSongController : Controller
+	{
+		public RequestSongController()
+		{
+
+		}
+
+		private List<RequestSong> RequestSongs { get; } = new()
+		{
+			new RequestSong { Id = 1, Sort = 1, Path = @"D:\Song\hoge.mp4", SongName = "HogeS", TieUpName = "HogeAnime", User = "Taro" },
+			new RequestSong { Id = 2, Sort = 2, Path = @"D:\Song\fuga.mp4", SongName = "FugaS", TieUpName = "FugaAnime", User = "Hanako" },
+			new RequestSong { Id = 3, Sort = 3, Path = @"D:\Song\foo.mp4", SongName = "FooS", TieUpName = "FooAnime", User = "Miyuki" },
+		};
+
+		[HttpGet]
+		public IEnumerable<RequestSong> GetDevices()
+		{
+			return RequestSongs;
+		}
+
+#if false
+		public IActionResult Index()
+		{
+			return View();
+		}
+#endif
+	}
+}
